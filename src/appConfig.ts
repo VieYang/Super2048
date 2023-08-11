d56c016e97bfa8ae75ad95912ca9409d72f09e9a
Vie Yang <yangchenzhong@gmail.com>
2023-08-11 14:15:13 +0800

    add superchain support

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,12 +1,11 @@
 export const app: { [key: string]: string } = {
-  name: 'dApp Starter', // web app name
-  title: 'dApp Starter Boilerplate', // website title
-  description: 'dApp Starter Boilerplate by https://github.com/arisac', // description of website
-  keywords: 'web3, dapp, ethereum, ethers, wagmi, rainbowkit, nextjs, react, tailwindcss, sass, boilerplate', // keywords for SEO
-  url: 'https://dapp-starter.aris.ac', // website url
+  name: 'EthGlobal Superhack Super2048', // web app name
+  title: 'EthGlobal Superhack Super2048', // website title
+  description: 'EthGlobal Superhack Super2048 by https://github.com/vieyang', // description of website
+  keywords: 'ethglobal, superhack, super2048, 2048, web3, dapp, ethereum, optimism, base, zora', // keywords for SEO
+  url: 'https://ethglobal.com/showcase/super2048-3hvmz', // website url
   image: '/icon-512x512.png', // image url for SEO
   favicon: '/favicon.ico', // favicon url
-  twitter: '@arisdotac', // twitter account
   themeColor: '#FFFFFF', // theme color
   themeColorDark: '#000000', // theme color dark
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,22 +7,12 @@ import { app } from 'appConfig'
 import { useState, useEffect } from 'react'
 import HeadGlobal from 'components/HeadGlobal'
 // Web3Wrapper deps:
-import { connectorsForWallets, RainbowKitProvider, lightTheme, darkTheme } from '@rainbow-me/rainbowkit'
-import {
-  injectedWallet,
-  metaMaskWallet,
-  braveWallet,
-  coinbaseWallet,
-  walletConnectWallet,
-  ledgerWallet,
-  rainbowWallet,
-} from '@rainbow-me/rainbowkit/wallets'
-import { Chain } from '@rainbow-me/rainbowkit'
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains'
-import { createClient, configureChains, WagmiConfig } from 'wagmi'
-import { infuraProvider } from 'wagmi/providers/infura'
+import { getDefaultWallets, RainbowKitProvider, lightTheme, darkTheme } from '@rainbow-me/rainbowkit'
+import { mainnet, optimism, base, zora, optimismGoerli } from 'wagmi/chains'
+import { createConfig, configureChains, WagmiConfig } from 'wagmi'
+// import { infuraProvider } from 'wagmi/providers/infura'
+// import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -37,74 +27,23 @@ function App({ Component, pageProps }: AppProps) {
 }
 export default App
 
-// Add Custom Chain
-// Demo purpose, gnosis is included in wagmi/chains
-// import { gnosis } from 'wagmi/chains'
-const gnosisChain: Chain = {
-  id: 100,
-  name: 'Gnosis',
-  network: 'gnosis',
-  iconUrl: 'https://uploads-ssl.webflow.com/63692bf32544bee8b1836ea6/637b0145cf7e15b7fbffd51a_favicon-256.png',
-  iconBackground: '#000',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'Gnosis',
-    symbol: 'xDAI',
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://gnosischain-rpc.gateway.pokt.network'],
-    },
-  },
-  blockExplorers: {
-    etherscan: {
-      name: 'Gnosisscan',
-      url: 'https://gnosisscan.io/',
-    },
-    default: {
-      name: 'Gnosis Chain Explorer',
-      url: 'https://blockscout.com/xdai/mainnet/',
-    },
-  },
-  testnet: false,
-}
-
 // Web3 Configs
-const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, gnosisChain],
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, optimism, base, zora, optimismGoerli],
   [
-    infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_ID !== '' && process.env.NEXT_PUBLIC_INFURA_ID }),
-    jsonRpcProvider({
-      rpc: chain => {
-        if (chain.id !== gnosisChain.id) return null
-        return {
-          http: `${chain.rpcUrls.default}`,
-        }
-      },
-    }),
+    // infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_ID !== '' && process.env.NEXT_PUBLIC_INFURA_ID }),
+    // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID !== '' && process.env.NEXT_PUBLIC_ALCHEMY_ID }),
     publicProvider(),
   ]
 )
 
-const otherWallets = [
-  braveWallet({ chains }),
-  ledgerWallet({ chains }),
-  coinbaseWallet({ chains, appName: app.name }),
-  rainbowWallet({ chains }),
-]
+const { connectors } = getDefaultWallets({
+  appName: app.name,
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
+  chains,
+})
 
-const connectors = connectorsForWallets([
-  {
-    groupName: 'Recommended',
-    wallets: [injectedWallet({ chains }), metaMaskWallet({ chains }), walletConnectWallet({ chains })],
-  },
-  {
-    groupName: 'Other Wallets',
-    wallets: otherWallets,
-  },
-])
-
-const wagmiClient = createClient({ autoConnect: true, connectors, provider })
+const wagmiConfig = createConfig({ autoConnect: true, publicClient, webSocketPublicClient, connectors })
 
 // Web3Wrapper
 export function Web3Wrapper({ children }) {
@@ -115,14 +54,14 @@ export function Web3Wrapper({ children }) {
   if (!mounted) return null
 
   return (
-    <WagmiConfig client={wagmiClient}>
+    <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider
         appInfo={{
           appName: app.name,
           learnMoreUrl: app.url,
         }}
         chains={chains}
-        initialChain={1} // Optional, initialChain={1}, initialChain={chain.mainnet}, initialChain={gnosisChain}
+        initialChain={optimismGoerli.id} // Optional, initialChain={1}, initialChain={chain.mainnet}, initialChain={gnosisChain}
         showRecentTransactions={true}
         theme={resolvedTheme === 'dark' ? darkTheme() : lightTheme()}
       >
